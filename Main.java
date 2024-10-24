e638781ac50e9889f719362c1dd646f9db08d53f
Larinayaa <kovalcukanna10@gmail.com>
2024-10-24 17:01:22 +0300

    Добавила цикл. Готово

--- Main.java
+++ Main.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Каким способом вы хотите отправить письмо?");
-        System.out.println("1 - DHL");
-        System.out.println("2 - E-Mail");
-        System.out.println("3 - Owl");
-        int number = scanner.nextInt();
         MailDeliveryService typDelivery = null;//создали пустую переменную чтоб потом присвоить ей одно из необходимых значений
+         while (typDelivery==null){ //пока неправильно, запрашивать выбор
+             System.out.println("Каким способом вы хотите отправить письмо?");
+             System.out.println("1 - DHL");
+             System.out.println("2 - E-Mail");
+             System.out.println("3 - Owl");
+        int number = scanner.nextInt();
         if (number==1) {
             typDelivery = new DHL();
         } else if (number==2) {
@@ -18,10 +19,11 @@ public class Main {
         } else {
 
             System.out.println("Ошибка! Неправильный тип доставки. Повторите ввод: ");
-        }
+        }}
         if (typDelivery!=null){
             Sender sender = new Sender();
             Sender.send(typDelivery);
         }
+        System.out.println("Хорошего дня!");
     }
 }
